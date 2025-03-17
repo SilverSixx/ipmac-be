a3d1216840e93a5293653deaf37f485cd37f07b5
SilverSixx <phamluongdat231103@gmail.com>
2025-03-17 21:53:08 +0700

    add test in ci

--- training-service/src/test/java/dev/datpl/trainingservice/controller/TrainingControllerTest.java
+++ training-service/src/test/java/dev/datpl/trainingservice/controller/TrainingControllerTest.java
@@ -11,8 +11,7 @@ import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TrainingControllerTest {
